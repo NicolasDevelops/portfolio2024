1801eb728e7f88f91be33dd5947fc6c8e9a4d420
NicolasDevelops <nicolasortiz.dev@hotmail.com>
2024-04-15 10:52:03 -0400

    final optimization - maybe

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,3 +1,28 @@
+
+// Intersection Observer API FadeUp Animation
+
+// Sets the observer's options
+const observerOptions = {
+  threshold: 0.1 // Adjust the threshold as per your requirement
+};
+
+// Creates observer to the target (section) to add the in-view class to the section element when we want to intersect.
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+      observer.unobserve(entry.target);
+    }
+  });
+}, observerOptions);
+
+// Adds DOMContentLoaded to make sure elements have been loaded in the DOM.
+window.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.section.fadeUp');
+  sections.forEach(section => observer.observe(section));
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
 	const CSbody = document.querySelector("body");
 	const CSnavbarMenu = document.querySelector("#cs-navigation");
