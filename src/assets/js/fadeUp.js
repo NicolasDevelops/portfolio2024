4a982a6fd405d8244c3375158b5a23388abca7f9
NicolasDevelops <nicolasortiz.dev@hotmail.com>
2024-04-15 10:10:42 -0400

    optimization test

--- src/assets/js/fadeUp.js
+++ src/assets/js/fadeUp.js
@@ -2,27 +2,21 @@
 
 // Sets the observer's options
 const observerOptions = {
-	root: null,
-	threshold: 0,
-	rootMargin: '0px 0px -50px 0px' // Adjust the margin as per your requirement
+  threshold: 0.1 // Adjust the threshold as per your requirement
 };
 
 // Creates observer to the target (section) to add the in-view class to the section element when we want to intersect.
-
 const observer = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('in-view');
-			observer.unobserve(entry.target);
-		}
-	});
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+      observer.unobserve(entry.target);
+    }
+  });
 }, observerOptions);
 
-// adds DOMContentLoaded to make sure elements have been loaded in the DOM.
-// Observes every element that has the 'section' class. 
-window.addEventListener('DOMContentLoaded', (event) => {
-	const sections = Array.from(document.getElementsByClassName('section'));
-	for (let section of sections) {
-		observer.observe(section);
-	}
+// Adds DOMContentLoaded to make sure elements have been loaded in the DOM.
+window.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('.section.fadeUp');
+  sections.forEach(section => observer.observe(section));
 });
