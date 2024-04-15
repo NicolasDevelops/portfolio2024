9c5a27994ee61295996fa0f5e2e090ab72329baa
NicolasDevelops <nicolasortiz.dev@hotmail.com>
2024-04-15 12:24:09 -0400

    optimization

--- src/assets/js/hero-fadeUp.js
+++ src/assets/js/hero-fadeUp.js
@@ -1,22 +1,27 @@
 // Intersection Observer API FadeUp Animation
 
 // Sets the observer's options
-const heroObserverOptions = {
+const HeroObserverOptions = {
   threshold: 0.1 // Adjust the threshold as per your requirement
 };
 
 // Creates observer to the target (section) to add the in-view class to the section element when we want to intersect.
-const heroObserver = new IntersectionObserver(entries => {
+const HeroObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('hero-in-view');
-      heroObserver.unobserve(entry.target);
+      entry.target.classList.add('heroInView');
+      HeroObserver.unobserve(entry.target);
     }
   });
-}, heroObserverOptions);
+}, HeroObserverOptions);
 
 // Adds DOMContentLoaded to make sure elements have been loaded in the DOM.
 window.addEventListener('DOMContentLoaded', () => {
-  const heroSection = document.getElementById('hero-1426');
-  heroObserver.observe(heroSection);
+  const sections = document.querySelectorAll('.heroSection.heroFadeUp');
+  sections.forEach(section => {
+    HeroObserver.observe(section);
+    
+    // Apply the 'fadeUp' class dynamically after initial content loading
+    section.classList.add(".heroInView");
+  });
 });
